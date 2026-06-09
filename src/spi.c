02e27962a2a770d1b8cb8c9beb311b46d02d483d
Ed Beroset <beroset@ieee.org>
2026-06-09 19:35:56 -0400

    Correct SPI settings
    This fixes the SPI_read function by making sure it's ok to transmit
    before we try to read.  Not sure it's needed.
    
    Signed-off-by: Ed Beroset <beroset@ieee.org>

--- src/spi.c
+++ src/spi.c
@@ -3,9 +3,14 @@
 #include "pindefs.h"
 // SPI CR1
 #define SPE         6
+#define BR2         5
+#define BR1         4
 #define BR0         3
 #define MSTR        2
+#define CPOL        1
+#define CPHA        0
 // SPI CR2
+#define BDM         7 // 0=2-line unidirectional mode
 #define SSM         1
 #define SSI         0
 // SPI SR
@@ -13,7 +18,7 @@
 #define TXE         1
 #define RXNE        0
 
-/* 
+/*
  * I found this web site useful and have based much of this code on it:
  * https://lujji.github.io/blog/bare-metal-programming-stm8/
  */
@@ -25,8 +30,8 @@ void SPI_init()
     SCR1(CS);
     SET(CS);
     // set up SPI mode
-    SPI_CR2 = (1u << SSM) | (1u << SSI);
     SPI_CR1 = (1u << SPE) | (1u << BR0) | (1u << MSTR);
+    SPI_CR2 = (0u << SSM) | (1u << SSI);
 }
 
 void SPI_write(uint8_t data)
@@ -37,6 +42,7 @@ void SPI_write(uint8_t data)
 
 uint8_t SPI_read()
 {
+    while (!(SPI_SR & (1 << TXE)));
     SPI_write(0xFF);
     while (!(SPI_SR & (1 << RXNE)));
     return SPI_DR;
